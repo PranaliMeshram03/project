92b6ef1afac0ad9fb2557f1ce6740c50383d7c9c
PranaliMeshram03 <130482239+PranaliMeshram03@users.noreply.github.com>
2023-04-17 18:10:26 +0530

    firstcommit

---
+++ javaProjrct/src/javaProjrct/DivisbleNumber.java
@@ -0,0 +1,19 @@
+package javaProjrct;
+
+public class DivisbleNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int a = 1; a <= 100; a++) {
+		    if (a % 5 == 0) {
+		       System.out.println(a / 5);
+		    } else if (a % 10 == 0) {
+		    	System.out.println(a / 10);
+		    } else {
+		    	System.out.println(a);
+		    }
+		}
+
+	}
+
+}
